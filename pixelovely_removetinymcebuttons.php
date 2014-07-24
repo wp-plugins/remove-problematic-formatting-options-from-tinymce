d7f74aed95c997e0c1c13bd10e6d801ae0009a51
PIXELovely <PIXELovely@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-24 16:33:13 +0000

    First commit of plugin
    git-svn-id: https://plugins.svn.wordpress.org/remove-problematic-formatting-options-from-tinymce/trunk@954267 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pixelovely_removetinymcebuttons.php
@@ -0,0 +1,55 @@
+<?php
+/*
+	Plugin Name: Remove Problematic Formatting Options From tinyMCE
+	Plugin URI: http://www.pixelovely.com/format-web-content-wordpress/
+	Description: Removes those extra-tempting but extra-problematic buttons from tinyMCE, like font color, alignment, and other things that will wreck your style sheet. Encourages the use of the "role" dropdown.
+	Author: PIXELovely
+	Version: 1.0.0
+	Author URI: http://www.PIXELovely.com/
+ */
+
+/*  Copyright 2014  Kimberly Genly (email : kim@pixelovely.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+	You may find a copy of the GNU General Public License at http://www.gnu.org/licenses/gpl.html
+*/
+ 
+if (isset($wp_version)) {
+	add_filter("mce_buttons", "pix_extended_editor_mce_buttons", 0);
+	add_filter("mce_buttons_2", "pix_extended_editor_mce_buttons_2", 0);
+}
+
+function pix_extended_editor_mce_buttons($buttons) {
+return array(
+"undo", "redo", "separator",
+"bold", "italic", "underline", "strikethrough", "separator",
+"bullist", "numlist", "separator",
+"charmap", "separator", "link", "unlink", "anchor", "blockquote", 
+"separator", 
+"formatselect", 
+"separator", 
+"removeformat");
+}
+
+function pix_extended_editor_mce_buttons_2($buttons) {
+// the second toolbar line
+return array();
+}
+
+
+function pix_remove_h1($init) {
+  $init['block_formats'] = "Paragraph=p; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6; Address=address; Pre=pre; Code=code";
+  return $init;
+}
+
+add_filter('tiny_mce_before_init', 'pix_remove_h1' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Plugin Name ===
+Contributors: PIXELovely
+Donate link: http://pixelovely.com/donate
+Tags: SEO, tinymce
+Requires at least: 3.0.1
+Tested up to: 3.9.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Removes those extra-tempting but extra-problematic buttons from tinyMCE, like font color, alignment, and other things that will wreck your style sheet.
+
+In addition, it removes "Heading 1" from the role dropdown, as there should never be more than one H1 on a page -- and the page or post title is already it.
+
+For more information on why formatting matters to brand consistency, SEO, accessibility and even maintainability, check out this blog post: http://www.pixelovely.com/format-web-content-wordpress 
+
+== Changelog ==
+
+= 1.0.0 =
+* Plugin first released! I would very much appreciate it if you remembered to rate the plugin after trying it.
